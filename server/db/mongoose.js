7c5d96c11d9cc22393ed764a50fb03138d716300
Jagdish Sharma <jagdishcsharma29@gmail.com>
2018-11-10 20:26:47 +0530

    changed

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,11 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+let db = {
+  mlab: 'mongodb://jagdish:adobemuse12@ds159273.mlab.com:59273/first_mongodb',
+  localhost: 'mongodb://localhost:27017/TodoApp',
+};
+mongoose.connect(db.localhost || db.mlab);
 
 module.export = {
   mongoose
