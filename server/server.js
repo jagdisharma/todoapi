842fe32d90603705965b9110af10237241ce5949
Jagdish Sharma <jagdishcsharma29@gmail.com>
2018-11-10 10:50:44 +0530

    Add get todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,16 @@ app.post('/todos', (req, res)=> {
   })
 });
 
+app.get('/todos', (req, res) =>{
+  Todo.find().then( (todos) => {
+    //console.log('Inside response');
+    res.send({todos});
+  }, (e) => {
+    //console.log('Error');
+    res.status(400).send(e);
+  })
+});
+
 app.listen(3000, () => {
   console.log('Listening to port number 3000...');
 });
